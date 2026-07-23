ee894c36ac7dcb2a3abf9518610e861f5f8c72a3
spareform007 <spareform007@gmail.com>
2026-07-23 10:28:25 +0530

    Delete ECOMMERECE IN COSMETICS/cosmetics directory

--- ECOMMERECE IN COSMETICS/cosmetics/server/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getAIRecommendations } from '../controllers/aiController.js';
-
-const router = express.Router();
-
-router.post('/recommend', getAIRecommendations);
-
-export default router;
