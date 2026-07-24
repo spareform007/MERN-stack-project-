97a6bc08213526f6471882ed17467b7900086a1d
spareform007 <spareform007@gmail.com>
2026-07-24 09:58:01 +0530

    Update vite.config.js

--- ECOMMERCE IN COSMETICS/cosmetics/client/vite.config.js
+++ ECOMMERCE IN COSMETICS/cosmetics/client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://mern-stack-project-gbqb.onrender.com', //http://localhost:5000
         changeOrigin: true
       }
     }
